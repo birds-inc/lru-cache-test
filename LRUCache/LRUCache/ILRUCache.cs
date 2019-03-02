84a9b36b520a305895eb5cd142c389f0eb469f31
Alex Gallichotte <agallichotte@nationalresearch.com>
2019-03-02 13:26:15 -0800

    define interface

---
+++ LRUCache/LRUCache/ILRUCache.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LRUCache {
+    interface ILRUCache {
+        byte[] Get(byte[] key);
+        void Put(byte[] key, byte[] value);
+        bool Contains(byte[] key);
+        void Delete(byte[] key);
+    }
+}
---
+++ LRUCache/LRUCache/LRUCache.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LRUCache
+{
+    public class LRUCache : ILRUCache
+    {
+        public byte[] Get(byte[] key) {
+            throw new NotImplementedException();
+        }
+
+        public void Put(byte[] key, byte[] value){
+            throw new NotImplementedException();
+        }
+        
+        public bool Contains(byte[] key){
+            throw new NotImplementedException();
+        }
+
+        public void Delete(byte[] key){
+            throw new NotImplementedException();
+        }
+    }
+}
